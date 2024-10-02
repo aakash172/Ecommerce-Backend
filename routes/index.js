fbb5d362315a0437cd5b369c1bf5018e0305088e
aakash <aakashchaudhary172m@gmail.com>
2024-10-03 02:34:15 +0530

    completed admin panel

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ const authToken = require("../middleware/authToken.js");
 const userDetailsController = require("../controller/userDetails.js");
 const userLogout = require("../controller/userLogout.js");
 const allUsers = require("../controller/allUsers.js");
+const updateUser = require("../controller/updateUser.js");
 
 router.post("/signup", userSignUpController);
 router.post("/signin", userSignInController);
@@ -15,5 +16,6 @@ router.get("/user-details", authToken, userDetailsController);
 router.get("/userLogout", userLogout);
 
 router.get("/all-user", authToken, allUsers);
+router.post("/update-user", authToken, updateUser);
 
 module.exports = router;
